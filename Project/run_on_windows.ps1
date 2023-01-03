d8229ec21ac68ce9b9a72e83fb1842f07d71f910
GiorgioPerna-2 <giorgio.perna905@edu.unito.it>
2023-01-03 17:11:56 +0100

    aggiunta dockerrizzazione servizi netflix e prime

--- Project/run_on_windows.ps1
+++ Project/run_on_windows.ps1
@@ -2,13 +2,13 @@
 cd user-service
 mvn clean 
 mvn install -DskipTests
-docker build -t user-service . 
+docker build -t user-service .
 cd .. 
 
 cd service-discovery-server 
 mvn clean 
 mvn install -DskipTests
-docker build -t service-discovery-server . 
+docker build -t service-discovery-server .
 cd .. 
 
 cd api-gateway 
@@ -29,4 +29,17 @@ mvn install -DskipTests
 docker build -t google-service .
 cd ..
 
+cd netflix-service
+mvn clean
+mvn install -DskipTests
+docker build -t netflix-service .
+cd ..
+
+cd prime-service
+mvn clean
+mvn install -DskipTests
+docker build -t prime-service .
+cd ..
+
+docker image prune -f
 docker compose up
